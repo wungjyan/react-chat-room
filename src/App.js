ebaf3e897982c2eef48f288ec193bd5bb93d18a2
wungjyan <1170017412@qq.com>
2019-01-08 13:21:25 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {connect} from 'react-redux';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state){
+  return {
+    num:state.num
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    handleChange(){
+      dispatch({type:"add",n:6})
+    }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+
+const initState = {
+	num:1
+}
+
+function reducer(state=initState,action){
+	return state
+}
+
+export default reducer;
