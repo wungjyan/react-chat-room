4e896e9310186a5dc13d48ac3687115379a82f93
wungjyan <1170017412@qq.com>
2019-01-08 19:55:55 +0800

    修改头部右侧信息逻辑

--- src/components/Btns.js
+++ src/components/Btns.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 class Btns extends Component {
   render() {
+    console.log("BTns:", this.props);
     return (
       <span className="btn">
-        {this.props.location.pathname === "/login" ? (
+        {this.props.location.pathname === "/chat" && this.props.nickname ? (
+          <span>{this.props.nickname}</span>
+        ) : this.props.location.pathname === "/login" ? (
           <span onClick={() => this.props.history.push("/register")}>注册</span>
         ) : (
           <span onClick={() => this.props.history.push("/login")}>登录</span>
@@ -14,4 +18,15 @@ class Btns extends Component {
   }
 }
 
-export default withRouter(Btns);
+const mapStateToProps = state => {
+  return {
+    nickname: state.nickname
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(Btns)
+);
