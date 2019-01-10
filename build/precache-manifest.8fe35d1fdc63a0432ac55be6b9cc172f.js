2ec3b7df9fe7a9ff03783b47bbad71d3939aac84
wungjyan <1170017412@qq.com>
2019-01-10 14:45:59 +0800

    上传打包文件

---
+++ build/precache-manifest.8fe35d1fdc63a0432ac55be6b9cc172f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02c8ed75c2739e5be7f8",
+    "url": "/static/js/main.02c8ed75.chunk.js"
+  },
+  {
+    "revision": "028821397f5e24cf74a1",
+    "url": "/static/js/1.02882139.chunk.js"
+  },
+  {
+    "revision": "02c8ed75c2739e5be7f8",
+    "url": "/static/css/main.cf9b0908.chunk.css"
+  },
+  {
+    "revision": "028821397f5e24cf74a1",
+    "url": "/static/css/1.73ce9802.chunk.css"
+  },
+  {
+    "revision": "6fa4229e5920855d21711d882df78b58",
+    "url": "/index.html"
+  }
+];
